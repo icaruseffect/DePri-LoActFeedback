2d75b27ed87c74905ccc231c7ddd2584cd61cfb7
Icaruseffect <icaruseffect@posteo.de>
2015-09-06 13:29:34 +0200

    angular service lädt  Json, Directive zum laden + Treemap

--- js/app.js
+++ js/app.js
@@ -1,13 +1,87 @@
-// Application Module
-var app = angular.module('myApp', ['jsonService']);
+/* @flow */
+// aktiviert den Flow-Linter
 
-//Main Controller
-app.controller('MainCtrl', function ($scope, JsonService) {
+// Definiere ( App-Name, [ Abhängigkeiten])
+var app = angular.module('app', ['dataService', 'EnergyTreemap']);
 
-  // Use the JsonService in services.js to move data from json to scope
-  JsonService.get(function (data) {
-    $scope.name = data.name;
-    $scope.children = data.children;
-  });
+/* Initialisierung Controller mit $scope und dataService
+ der Rückgabewert von dataService wird im Scope gespeichert */
+app.controller('MainCtrl', function ($scope, dataService) {
+
+  // This function gets the Data from the local json file
+  $scope.updateData = function () {
+    dataService.getData(function (data) {
+      $scope.data = data;
+      //$scope.children = data.children;
+      //Debug Ausgabe auf Konsole
+      console.log("Daten aktualisiert");
+    });
+  };
 
 });
+
+
+/*
+  Services
+*/
+// Data Service holt die Daten aus der json in js/ und gibt diese an den Aufrufe zurück
+// Definiere ( App-Name, [ Abhängigkeiten])
+var dataService = angular.module('dataService', ['ngResource']);
+
+dataService.factory('dataService', function ($resource) {
+  return $resource('js/data.json', {}, {
+    getData: {
+      method: 'GET',
+      isArray: false
+    }
+  });
+});
+
+
+/*
+  Directives
+*/
+var chart = angular.module('EnergyTreemap', [])
+  // D3 Factory
+chart.factory('d3', function () {
+  /* We could declare locals or other D3.js
+  specific configurations here. */
+  return d3;
+});
+chart.directive('zoomD3Treemap', ["d3",
+  function (d3) {
+    function draw(svg, width, height, data) {
+      svg
+        .attr('width', width)
+        .attr('height', height);
+      // code continues here
+    }
+
+
+
+    return {
+      restrict: 'E',
+      scope: {
+        data: '='
+      },
+      compile: function (element, attrs, transclude) {
+        // Create a SVG root element
+        var svg = d3.select(element[0]).append('svg');
+        svg.append('g').attr('class', 'data');
+        svg.append('g').attr('class', 'x-axis axis');
+        svg.append('g').attr('class', 'y-axis axis');
+        // Define the dimensions for the chart
+        var width = 600,
+          height = 300;
+        // Return the link function
+        return function (scope, element, attrs) {
+          // Watch the data attribute of the scope
+          scope.$watch('data', function (newVal, oldVal, scope) {
+            // Update the chart
+            draw(svg, width, height, scope.data);
+          }, true);
+        };
+      }
+    };
+  }
+]);
--- js/services.js
+++
@@ -1,4 +0,0 @@
-angular.module('myApp', ['ngResource'])
-  .factory('JsonService', function ($resource) {
-    return $resource('data.json');
-  });
